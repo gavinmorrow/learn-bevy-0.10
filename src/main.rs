6b7ceb167a78abd3eec669de7e1564d29ca1313c
Gavin Morrow <gavinfmorrow@gmail.com>
2023-07-12 11:37:42 -0400

    sample bevy code (save point)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl HelloPlugin {
+    fn greet_people(
+        time: Res<Time>,
+        mut timer: ResMut<GreetTimer>,
+        query: Query<&Name, With<Person>>,
+    ) {
+        if timer.0.tick(time.delta()).just_finished() {
+            for name in &query {
+                println!("hello {}!", name.0);
+            }
+        }
+    }
+}
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(HelloPlugin::greet_people);
+    }
 }
